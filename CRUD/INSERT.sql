862bd2e3c061d3e83fdfac1a4fb5e91e23708d37
lucas-bardeli <lucasbbsjau@gmail.com>
2025-10-29 19:36:36 -0300

    Comando insert com value e outras portas de banco

--- CRUD/INSERT.sql
+++ CRUD/INSERT.sql
@@ -2,11 +2,12 @@
 -- Comando INSERT, o C (Create) do CRUD
 -- Inserindo registros nas tabelas
 
-INSERT 
-INTO nome_tabela (coluna1, coluna2, coluna3, coluna4, ... ) 
+-- Apenas um registro, pode ser com VALUE no singular também
+INSERT INTO nome_tabela (coluna1, coluna2, coluna3, coluna4, ... ) 
 VALUES (valor1, valor2, valor3, valor4, ... );
 
 -- Múltiplos registros de uma vez:
+-- Obrigatoriamente com VALUES no plural
 INSERT INTO nome_tabela (coluna1, coluna2, coluna3, coluna4, ... ) VALUES
 (valor1, valor2, valor3, valor4, ... ),
 (valor1, valor2, valor3, valor4, ... ),
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## 🗃️ Banco de Dados Relacional:
+# 🗃️ Banco de Dados Relacional
 Comandos SQL das aulas de Modelagem de Banco de Dados e Banco de Dados Relacional da Fatec de Jahu.
